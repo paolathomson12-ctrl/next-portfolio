f343415178cb40f4641fa1add29cd2ae64b987a9
Paola D'Antonio <paola.dantonio12@gmail.com>
2026-07-23 11:47:56 +1000

    removed email

--- config/socials.ts
+++ config/socials.ts
@@ -13,11 +13,5 @@ export const SocialLinks: SocialInterface[] = [
     username: "Paola Thomson",
     icon: Icons.linkedin,
     link: "https://www.linkedin.com/in/paolathomson",
-  },
-  {
-    name: "Gmail",
-    username: "paola.thomson12",
-    icon: Icons.gmail,
-    link: "mailto:paola.thomson12@gmail.com",
-  },
+  }
 ];
